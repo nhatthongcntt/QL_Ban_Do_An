66093910136bccdfd6ab1d4c0a84d1fc42d5e788
VoThiKimGiau <vothikimgiau16122003@gmail.com>
2024-05-20 20:51:26 +0700

    test

--- app/src/main/java/com/example/ql_ban_do_an/MainActivity.java
+++ app/src/main/java/com/example/ql_ban_do_an/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.ql_ban_do_an;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
