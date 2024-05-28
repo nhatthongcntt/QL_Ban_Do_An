296b0599e1344072954c430dbde43d7b4ef99d81
LeKiet2711 <legiakiet03@gmail.com>
2024-05-28 21:12:05 +0700

    Cart

---
+++ app/src/main/java/com/example/ql_ban_do_an/Helper/ChangeNumberItemsListener.java
@@ -0,0 +1,5 @@
+package com.example.ql_ban_do_an.Helper;
+
+public interface ChangeNumberItemsListener {
+    void change();
+}
---
+++ app/src/main/java/com/example/ql_ban_do_an/View/CartActivity.java
@@ -0,0 +1,14 @@
+package com.example.ql_ban_do_an.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.ql_ban_do_an.R;
+public class CartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cart);
+    }
+}
