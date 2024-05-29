7534f3c46ed82691ffc81dd5c57a47e7a28e2842
LeKiet2711 <legiakiet03@gmail.com>
2024-05-29 10:32:40 +0700

    Cart

--- app/src/main/java/com/example/ql_ban_do_an/View/CartActivity.java
+++ app/src/main/java/com/example/ql_ban_do_an/View/CartActivity.java
@@ -1,10 +1,16 @@
 package com.example.ql_ban_do_an.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import com.example.ql_ban_do_an.Helper.ManagmentCart;
 import com.example.ql_ban_do_an.R;
 public class CartActivity extends AppCompatActivity {
+    //private ActivityCartBinding binding;
+    private RecyclerView.Adapter adapter;
+    private ManagmentCart managementCart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
